94b1eb3ce4eea7bc08c51a149b9242d4663073f5
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-06-24 21:16:02 +0100

    style: change home page UI

--- src/app/nav.js
+++ src/app/nav.js
@@ -7,17 +7,17 @@ export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 w-full z-99 h-[10vw] py-6">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="fixed top-0 w-full z-99 h-[10vw] py-6 bg-white/20 backdrop-blur-md shadow-md border-b border-black/10">
+      <div className="container mx-auto flex justify-between items-center mt-[-3vh]">
 
         {/* Logo */}
-        <Link href="/" className="text-lg font-semibold text-black ml-4 bg-white/20 backdrop-blur-md rounded-full shadow-sm py-2 px-2">
+        <Link href="/" className="text-lg font-semibold text-black ml-4">
           <Image src="/logo.png" alt="cactivia logo" width={100} height={100} />
         </Link>
 
         {/* Hamburger Icon */}
         <button
-          className="sm:hidden flex flex-col justify-center items-center w-10 h-10 mr-4 bg-white/20 backdrop-blur-md rounded-xl shadow-sm transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#52030C]"
+          className="sm:hidden flex flex-col justify-center items-center w-10 h-10 mr-4"
           onClick={() => setMenuOpen(!menuOpen)}
           aria-label="Toggle menu"
         >
@@ -35,22 +35,22 @@ export default function Nav() {
           } sm:static sm:flex-row sm:bg-transparent sm:space-x-6`}
         >
           <li>
-            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 py-2 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
+            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
               Home
             </Link>
           </li>
           <li>
-            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 py-2 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
+            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
               Products
             </Link>
           </li>
           <li>
-            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 py-2 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
+            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#52030c] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
               About Us
             </Link>
           </li>
           <li>
-            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 py-2 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
+            <Link href="/" className="text-white rounded-full border border-black border-2 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] text-background hover:bg-[#59760b] font-large sm:text-base h-10 sm:h-12 px-4 sm:w-auto mx-auto sm:mx-0 cursor-pointer">
               Contact Us
             </Link>
           </li>
--- src/app/page.js
+++ src/app/page.js
@@ -23,65 +23,112 @@ export default function Home() {
       icon: <FaCircleCheck />,
     },
     {
-      id: 1,
+      id: 3,
       name: "Sustainable",
       description: "Promotes eco-friendly gardening.",
       icon: <FaLeaf />,
     }
   ];
+
+  let products = [
+    {
+      id: 1,
+      name: "Cactivia Original",
+      image: "/productA.jpg",
+      price: "19.99 TND",
+    },
+    {
+      id: 2,
+      name: "Cactivia Spray",
+      image: "/productA.jpg",
+      price: "12.99 TND",
+    },
+    {
+      id: 3,
+      name: "Cactivia Plus",
+      image: "/productA.jpg",
+      price: "24.99 TND",
+    },
+  ];
+
   return (
-    <div>
+    <div className="bg-[#daa547]">
 
-      <main className="text-center py-[20vh] flex flex-col items-center bg-[#daa547] px-[15vw] w-full">
+      <main className="text-center py-[20vh] flex flex-col items-center px-[15vw] w-full bg-[url('/solution-img.jpg')] bg-cover bg-center bg-no-repeat h-[90vh]">
 
-        <h3 className="text-sm">Treat Your Plants With Care</h3>
-        <h1 className="text-2xl sm:text-7xl text-black font-black mt-2">
-          Cactivia is your go-to solution for protecting prickly pears from the cochineal insect.
-        </h1>
+        <div className="bg-white/20 backdrop-blur-md rounded-xl shadow-[0_7px_0_0_black] p-4 flex flex-col items-center gap-2 w-full max-w-3xl mt-auto">
+          <h3 className="text-sm">Treat Your Plants With Care</h3>
+          <h1 className="text-2xl sm:text-7xl text-black font-black mt-2">
+            Cactivia is your go-to solution for protecting prickly pears from the cochineal insect.
+          </h1>
 
-        <div className="flex flex-row gap-4 mt-4">
+          <div className="flex flex-row gap-4 mt-4">
 
-          <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[5px] flex items-center gap-2">
-            See More <FaCircleArrowDown />
-          </button>
+            <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[5px] flex items-center gap-2">
+              See More <FaCircleArrowDown />
+            </button>
 
-          <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#daa547] font-medium px-[5px]">
-            Contact Us
-          </button>
+            <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-transparent font-medium px-[5px]">
+              Contact Us
+            </button>
 
+          </div>
         </div>
 
       </main>
+
+      {/* products */}
+
+      <div className="flex flex-col items-center gap-5 mt-10 text-black">
+        <h2 className="font-bold">Products Listing</h2>
+
+        {products.map((product) => (
+          <div key={product.id} className="bg-[#ebbc6a] rounded-xl shadow-[0_7px_0_0_black] p-2 flex items-center justify-between gap-2 w-[80vw]">
+            <div className="flex items-center gap-4">
+              <Image src={product.image} alt={product.name} width={50} height={50} className="rounded-xl" />
+              <div className="flex flex-col items-start">
+                <h3 className="text-lg font-semibold">{product.name}</h3>
+                <span className="text-sm opacity-75 font-bold">{product.price}</span>
+              </div>
+            </div>
+            <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[10px] text-black mt-2">
+              Buy
+            </button>
+          </div>
+        ))}
+
+      </div>
 
       {/* problem & solution */}
 
       <div className="flex flex-col items-center gap-5 mt-10 text-white">
 
-        <div className="bg-[#52030C] px-4 py-4 rounded-xl flex flex-col gap-2 mx-[10vw] border border-black border-3 shadow-[0_7px_0_0_black]">
-          <Image src="/problem-img.jpg" alt="Cactivia Logo" width={300} height={300} className="rounded-xl" />
+        <div className="bg-[url('/problem-img.jpg')] bg-cover bg-center bg-no-repeat">
+          <div className="flex flex-col gap-2 bg-[#59760b] opacity-90 px-10 py-4">
 
-          <div className="flex items-center gap-2">
-            <MdReportProblem />
-            <h2 className="font-bold">The Problem</h2>
+            <div className="flex items-center gap-2">
+              <MdReportProblem />
+              <h2 className="font-bold">The Problem</h2>
+            </div>
+
+            <p className="text-sm opacity-70 mt-2">
+              The cochineal insect is spreading fast across Tunisia, destroying prickly pear plants by sucking the sap and weakening entire crops.
+            </p>
+
+
+            <div className="flex items-center gap-2 mt-2">
+              <FaCircleCheck />
+              <h2 className="font-bold">Our solution</h2>
+            </div>
+
+            <p className="text-sm opacity-70 mt-2">
+              Our product offers a fast, effective, and environmentally safe way to eliminate the cochineal insect threatening prickly pear crops.
+            </p>
+          
+            <Image src={"/packaging.jpg"} width={500} height={500} className="my-4 shadow-[0_7px_0_0_black] rounded-xl"/>
+
           </div>
 
-          <p className="text-sm opacity-75">
-            The cochineal insect is spreading fast across Tunisia, destroying prickly pear plants by sucking the sap and weakening entire crops.<br />What starts as a few white spots quickly turns into a full-blown infestation, putting entire harvests at risk. With Care
-          </p>
-        </div>
-
-        <div className="bg-[#59760b] px-4 py-4 rounded-xl flex flex-col gap-2 mx-[10vw] border border-black border-3 shadow-[0_7px_0_0_black]">
-
-          <div className="flex items-center gap-2">
-            <FaCircleCheck />
-            <h2 className="font-bold">Our solution</h2>
-          </div>
-
-          <p className="text-sm opacity-75">
-            Our product offers a fast, effective, and environmentally safe way to eliminate the cochineal insect threatening prickly pear crops. By targeting the pest directly without harming the plant, our formula ensures healthier growth, better yields, and long-term protection.
-          </p>
-
-          <Image src="/solution-img.jpg" alt="Cactivia Logo" width={300} height={300} className="rounded-xl" />
         </div>
 
         <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[10px] text-black">
@@ -94,11 +141,11 @@ export default function Home() {
 
       <div className="flex flex-col items-center gap-5">
 
-        <div className="bg-[#A35227] px-4 py-4 rounded-xl flex flex-col items-center gap-2 mx-[10vw] border border-black border-3 shadow-[0_7px_0_0_black] mt-10 text-white">
+        <div className="px-4 py-4 flex flex-col items-center gap-2 mx-[10vw] mt-10">
           <h2 className="font-bold mb-2">What Makes Cactivia Special</h2>
           {specialTraitrs.map((trait) => (
-            <div key={trait.id} className="flex item-center gap-2 mx-2">
-              <div className="text-2xl p-5 bg-[#B86335] rounded-xl text-[#daa547]">{trait.icon}</div>
+            <div key={trait.id} className="flex item-center gap-4 mx-2">
+              <div className="text-2xl p-5 bg-[#ebbc6a] rounded-xl text-[#59760b]">{trait.icon}</div>
               <div>
                 <h3 className="font-semibold">{trait.name}</h3>
                 <p className="text-sm opacity-75">{trait.description}</p>
@@ -108,17 +155,13 @@ export default function Home() {
 
         </div>
 
-        <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[10px] text-black">
-          See Our Products
-        </button>
-
       </div>
 
       <Contact />
 
       <Footer />
-      
-    </div>
+
+    </div >
 
   );
 }
