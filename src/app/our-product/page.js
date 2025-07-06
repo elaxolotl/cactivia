eba537ccd075ec613556ee1b4d8080d3995c30dd
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-07-06 14:01:31 +0100

    feat: update product page

--- src/app/our-product/page.js
+++ src/app/our-product/page.js
@@ -1,60 +1,75 @@
-import Image from "next/image";
-import { FaCircleArrowDown} from "react-icons/fa6";
 import Contact from "../contact";
 import Footer from "../footer";
+import { FaWater } from "react-icons/fa";
+import { FaSprayCanSparkles } from "react-icons/fa6";
+import { IoIosPartlySunny } from "react-icons/io";
+
+
 
 export default function Home() {
 
+    const steps = [
+        {
+            id: 1,
+            icon: <FaWater size={30} />,
+            title: "Dilute with water",
+            description: "Dilute 200 ml of concentrate in 800 ml of water"
+        },
+        {
+            id: 2,
+            icon: <FaSprayCanSparkles size={30} />,
+            title: "Spray",
+            description: "Spray directly on both sides of leaves, stems, and fruits"
+        },
+        {
+            id: 3,
+            icon: <IoIosPartlySunny size={30} />,
+            title: "Know when to apply",
+            description: "Use in the early morning or late afternoon for best results"
+        },
+    ];
+
+
     return (
-        <div className="bg-[#daa547]">
+        <div className="bg-[#F6F6F6]">
 
-            <main className="text-center py-[20vh] flex flex-col items-center px-[15vw] w-full h-[90vh]">
-
-                <div className="bg-[url('/product-image.jpg')] rounded-xl shadow-[0_7px_0_0_black] p-4 flex flex-col items-center gap-2 w-full max-w-3xl mt-auto bg-cover bg-center bg-no-repeat">
-                    <h3 className="text-sm">Our Story</h3>
-                    <h1 className="text-2xl sm:text-7xl text-black font-black mt-2">
-                        We are a team of passionate individuals united by our commitment to sustainability.
-                    </h1>
-
-                    <div className="flex flex-row gap-4 mt-4">
-
-                        <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[5px] flex items-center gap-2">
-                            See More <FaCircleArrowDown />
-                        </button>
-
-                        <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-transparent font-medium px-[5px]">
-                            Contact Us
-                        </button>
-
-                    </div>
-                </div>
+            <main className="bg-[url('/product-image.jpg')] py-[20vh] flex flex-col items-center px-[15vw] w-full h-[90vh] bg-cover bg-center bg-no-repeat">
 
             </main>
 
-            {/* how cactivia came to life */}
+            <div className="mx-[10vw]">
+                <div className="bg-[#d1d1d1]/20 backdrop-blur-md rounded-xl shadow-[0_7px_0_0_black] p-4 flex flex-col gap-2 w-full max-w-3xl mt-[-10vh]">
+                    <h3 className="text-lg font-bold">Cactivia</h3>
+                    <div className="flex sm:flex-row items-center justify-between gap-4 mb-2">
+                        <h3 className="font-bold">12.50TND</h3>
+                        <button className="rounded-full border border-black border-3 shadow-[0_5px_0_0_black] transition-colors bg-[#59760b] font-medium px-[10px] text-black act">
+                            Buy
+                        </button>
+                    </div>
+                    <p className="text-sm opacity-70">A powerful 5x concentrated, eco-friendly solution crafted to protect prickly pear crops from the devastating cochineal
+                        insect.</p>
+                </div>
 
-            <div className="flex flex-col items-center mt-10 text-black mx-[10vw]">
-                <h2 className="font-bold mb-2">How Cactivia Came to Life</h2>
-                <div className="flex flex-col items-center gap-4">
-                    <p className="text-center opacity-75"> Born out of a competition within the 3ZERO TBS club, our mission is to create eco-friendly and natural formulation for the cochineal insect
-                        , supporting both local farmers and the environment.
-                        At Cactivia, we strive to leave a meaningful impact on the world by promoting sustainable agricultural practices and
-                        fostering economic growth in Tunisia.</p>
-                    <Image src="/3zero-team.jpg" alt="cactivia" width={300} height={300} className="my-4 shadow-[0_7px_0_0_black] rounded-xl h-[25vh] object-cover" />
+                <div className="flex flex-col items-center mt-10 text-black">
+                    <h2 className="text-lg font-bold">How to use Cactivia</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                        {steps.map((step, index) => (
+                            <>
+                                <div className="text-[15vw] font-bold text-[#59760b] mb-[-8vh]">{String(index + 1).padStart(2, "0")}</div>
+                                <div key={step.id} className="p-5 rounded-lg bg-white shadow-md flex items-start gap-4">
+                                    <div className="flex-1">
+                                        <div className="flex items-center gap-2 mb-2">
+                                            <div className="text-2xl p-2 bg-[#d1d1d1] rounded-xl text-[#59760b]">{step.icon}</div>
+                                            <h3 className="text-lg font-semibold">{step.title}</h3>
+                                        </div>
+                                        <p className="text-gray-600 text-sm">{step.description}</p>
+                                    </div>
+                                </div>
+                            </>
+                        ))}
+                    </div>
                 </div>
             </div>
-
-            {/* our mission */}
-
-            <div className="flex flex-col items-center mt-10 text-black mx-[10vw]">
-                <Image src="/our-mission.png" alt="cactivia" width={300} height={400} className="my-4 shadow-[0_7px_0_0_black] rounded-xl h-[25vh] object-cover" />
-                <h2 className="font-bold mb-2">Our Mission</h2>
-                <div className="flex flex-col items-center gap-4 opacity-75">
-                    <p className="text-center"> At Cactivia, our mission is to revolutionize agriculture with sustainable, eco-friendly fertilizers made
-                        from natural resources. We aim to support local farmers, enhance crop health, and promote greener, more sustainable farming practices. Through innovation and dedication, we strive to improve the Tunisian economy while making a positive impact on the environment.</p>
-                </div>
-            </div>
-
 
             <Contact />
 
