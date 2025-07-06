fef154c9215774cc5621c0a12fadfe32eca1ad9a
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-07-06 14:11:12 +0100

    style: change contact & footer colors

--- src/app/contact.js
+++ src/app/contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
-        <div className="bg-[#daa547] p-4 mt-5">
-            <div className="flex flex-col m-4 bg-white rounded-xl gap-2 p-4">
+        <div className="bg-[#59760b] p-4 mt-5">
+            <div className="flex flex-col m-4 bg-[white] rounded-xl gap-2 p-4">
                 <h3 className="font-bold">Contact Us</h3>
                 <input type="text" className="bg-[#D9D9D9] px-2 rounded-lg" placeholder="Name" />
                 <input type="email" className="bg-[#D9D9D9] px-2 rounded-lg" placeholder="Email" />
--- src/app/footer.js
+++ src/app/footer.js
@@ -6,15 +6,17 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="bg-[#59760B] text-white py-6">
-            <div className="container mx-auto text-center">
-                <p className="text-sm mb-2">© 2025 Cactivia. All rights reserved.</p>
-                <div className="flex justify-center gap-2">
-                    <p>Follow Us:</p>
-                    <a href="https://www.facebook.com/profile.php?id=61575930785720" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
-                        <FaFacebookSquare size={24} /> </a>
-                    <a href="https://www.instagram.com/cactivia_/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
-                        <FaInstagram size={24} /> </a>
+        <footer className="bg-[url('/solution-img.jpg')] bg-cover bg-center bg-no-repeat text-white">
+            <div className="bg-[#22222220] backdrop-blur-sm z-0 rounded-none h-[100%] py-6">
+                <div className="container mx-auto text-center">
+                    <p className="text-sm mb-2">© 2025 Cactivia. All rights reserved.</p>
+                    <div className="flex justify-center gap-2">
+                        <p>Follow Us:</p>
+                        <a href="https://www.facebook.com/profile.php?id=61575930785720" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                            <FaFacebookSquare size={24} /> </a>
+                        <a href="https://www.instagram.com/cactivia_/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                            <FaInstagram size={24} /> </a>
+                    </div>
                 </div>
             </div>
         </footer>
